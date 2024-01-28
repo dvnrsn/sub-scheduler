6e1d771f9550ee5b07c5ef6963d305710702be7a
= <=>
2024-01-28 14:44:10 -0700

    Style and use react-datepicker to select multiple dates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { isEqual, subDays } from 'date-fns';
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Home() {
+  const [availableDates, setAvailableDates] = useState<Date[]>([]);
+  const highlightWithRanges = [
+    {
+      'react-datepicker__date--selected': availableDates,
+    },
+  ];
+
+  const handleDateChange = (date: Date | null) => {
+    if (!date) return;
+    console.log(date, availableDates);
+    if (availableDates.some((d) => isEqual(d, date))) {
+      setAvailableDates(availableDates.filter((d) => !isEqual(d, date)));
+    } else {
+      setAvailableDates([...availableDates, date]);
+    }
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DatePicker onChange={(date) => console.log(date)} inline />
+      <DatePicker
+        onChange={handleDateChange}
+        inline
+        highlightDates={highlightWithRanges}
+        selected={null}
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+      />
     </main>
   );
 }
